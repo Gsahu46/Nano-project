4d8c964b9c15182a88246d9d276d7f5e64ab68a6
Gsahu46 <gsahu4900@gmail.com>
2026-05-20 22:58:07 +0530

    Testing for Nano-Project.

---
+++ Nano_Project/src/Project/testing/Testing.java
@@ -0,0 +1,14 @@
+package Project.testing;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		System.out.println("------------------------------");
+		
+		System.out.println("Testing my new project!");
+		
+		System.out.println("------------------------------");
+
+	}
+
+}
---
+++ Nano_Project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Nano_Project {
+}
